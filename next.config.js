eb59c52503ebb8d366e8bc14256bd9d1ebb4db5e
ahmadzaib-official <ahmad.zaib@mentorsol.com>
2025-10-16 20:42:38 +0500

    refactor: Uncomment middleware logic and enhance route protection

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['async_hooks'],
+  },
+webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('async_hooks');
+    }
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
