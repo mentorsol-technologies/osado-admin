ba9a38f74d453cfac7dfda8ab980260b107c49ad
ahmadzaib-official <ahmad.zaib@mentorsol.com>
2025-10-16 21:10:10 +0500

    Add .cprc configuration file and update middleware and wrangler.toml settings

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,6 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  runtime: 'experimental-edge',
   matcher: [
     /*
       Protect all routes except for _next and static files
